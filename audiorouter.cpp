fc1d3b60ebc2bcb79b6622ef7b850f0ced0f1882
Andy <weiss@groovenet.ch>
2023-05-08 23:48:00 +0200

    removed ravenna stuff

--- audiorouter.cpp
+++ audiorouter.cpp
@@ -65,9 +65,6 @@ QStringList AudioRouter::listInputSoundDev(){
         if(audiodev.inputCount>0){
             snddevlist << devname;
         }
-        if(devname.contains("RAVENNA")){                                            // this is done because the Merging driver does not return a channelcount.
-            snddevlist << devname;
-        }
     }
     return snddevlist;
 }
@@ -80,10 +77,7 @@ QStringList AudioRouter::listOutputSoundDev(){
         devname = QString::fromStdString(audiodev.name);
         if(audiodev.outputCount>0){
             snddevlist << devname;
-        }
-        if(devname.contains("RAVENNA")){                                            // this is done because the Merging driver does not return a channelcount.
-            snddevlist << devname;
-        }
+        }  
     }
     return snddevlist;
 }
@@ -125,11 +119,6 @@ void AudioRouter::AddClockingDevice(int recordDevId, int playbackDevId, QString 
 
     if (channelCnt > 64){                                                             // important edit splitcomb.c line 34  #define MAX_CHANNELS from 16 to 64
         channelCnt = 64;                                                              // limit the number of channels acorrding to MAX_CHANNEL set
-    }
-    if (QString::fromStdString(recorddev.name).contains("RAVENNA")){
-        channelCnt =64;
-        recorddev.inputCount = 64;
-        playbackdev.outputCount = 64;
     }
     if (channelCnt == 0){
         m_lib->m_Log->writeLog(3,"AddClockingDevice: Device has either no input or no outputs!" );
@@ -261,11 +250,6 @@ void AudioRouter::addAudioDevice(int recordDevId, int playbackDevId, QString uid
 
     if (channelCnt > 64){                                                             // important edit splitcomb.c line 34  #define MAX_CHANNELS from 16 to 64
         channelCnt = 64;                                                              // limit the number of channels acorrding to MAX_CHANNEL set
-    }
-    if (QString::fromStdString(recorddev.name).contains("RAVENNA")){
-        channelCnt =64;
-        recorddev.inputCount = 64;
-        playbackdev.outputCount = 64;
     }
     if (channelCnt == 0){
         m_lib->m_Log->writeLog(3,"AddAudioDevice: Device has either no input or no outputs!" );
